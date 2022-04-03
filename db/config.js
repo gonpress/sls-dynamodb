89cf6d4e6304b3041c745ad62bf71b88c5244065
gonpress <gonpress@naver.com>
2022-04-03 17:42:35 +0900

    first commit

---
+++ db/config.js
@@ -0,0 +1,6 @@
+module.exports.db = {
+  region: 'localhost',
+  endpoint: 'http://localhost:8000',
+  accessKeyId: 'nh5k3o', // needed if you don't have aws credentials at all in env
+  secretAccessKey: 'd8c1e', // needed if you don't have aws credentials at all in env
+}
